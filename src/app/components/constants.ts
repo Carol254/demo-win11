5ada250a665fcbf9aebffae9c7af14b4de785e15
Caroline <wanguic49@gmail.com>
2025-03-22 11:21:12 +0300

    changes on the settings component

--- src/app/components/constants.ts
+++ src/app/components/constants.ts
@@ -2,6 +2,11 @@ export const Constants = {
     iconPaths:{
       desktop:{
         DEFAULT_WALLPAPER: '/images/desktop/win-11-1.webp',
+        WALLPAPER_1:'/images/desktop/bg-1.jpg',
+        WALLPAPER_2:'/images/desktop/bg-2.jpg',
+        WALLPAPER_3:'/images/desktop/bg-3.jpg',
+        WALLPAPER_4:'/images/desktop/bg-4.jpg',
+        WALLPAPER_5:'/images/desktop/bg-5.jpg'
       },
       taskbar:{
         WIN_LOGO: '/images/icons/windows.png',
--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Constants } from '../constants';
 
 @Component({
   selector: 'app-settings',
@@ -6,6 +7,50 @@ import { Component } from '@angular/core';
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
-export class SettingsComponent {
+export class SettingsComponent  implements OnInit{
 
+   icons:any = Constants.iconPaths;
+
+  backgroundImages:any[] = [
+    {
+      id: 0,
+      url:this.icons.desktop.DEFAULT_WALLPAPER,
+      imgName:'defaultWallpaper' 
+    },
+    {
+      id: 1,
+      url:this.icons.desktop.WALLPAPER_1,
+      imgName:'wallpaper1' 
+    },
+    {
+      id: 2,
+      url:this.icons.desktop.WALLPAPER_2,
+      imgName:'wallpaper2' 
+    },
+    {
+      id: 3,
+      url:this.icons.desktop.WALLPAPER_3,
+      imgName:'wallpaper3' 
+    },
+    {
+      id: 4,
+      url:this.icons.desktop.WALLPAPER_4,
+      imgName:'wallaper4' 
+    },
+    {
+      id: 5,
+      url:this.icons.desktop.WALLPAPER_5,
+      imgName:'wallpaper5' 
+    },
+  ];
+
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+
+  loadBackgrounds(){
+
+  }
 }
